4e20c910aa0e094b5233f813201b0297e1902593
Lanndon Rose <roselan@mail.gvsu.edu>
2018-03-23 01:52:24 -0400

    Update pygame.py
    this is the working game

--- pygame.py
+++ pygame.py
@@ -13,7 +13,8 @@ import math
 if __name__ == "__main__":
     print("Zork, A Text Based RPG")
     print("The goal is to go to every home in the neighborhood and remove")
-    print("all of the monsters inside. Once this is done you win!")
+    print("all of the monsters inside. Once this is done, choose the option check. \n")
+    print("if all of the monsters are dead, you win. If they aren't. Too bad punk, you lose.")
 
 
     n = Neighborhood()
@@ -21,7 +22,7 @@ if __name__ == "__main__":
 
     hero = Player()
     candyBucket = hero.get_inventory()
-    
+
     #spacing
     print()
     print()
@@ -31,14 +32,17 @@ if __name__ == "__main__":
         int(houseNum)
         houseNum += -1
         hood[houseNum].get_NPC()
+        print()
+        print()
     else:
         print("You picked something that doesn't exsist. You're a dummy. \n \n GAME OVER")
         sys.exit()
 
 
-    while(houseNum != -1):
-
-        option = input("Do you want to 'attack', 'leave','inventory','sugar coma', 'check'? ")
+    while(len(hood)!= 0):
+        option = input("Do you want to 'attack', 'leave','inventory','sugar coma'?")
+        while(option is None):
+            option = input("Do you want to 'attack', 'leave','inventory','sugar coma'?")
 #Attack Command Options
         if option == "attack":
             npc = hood[houseNum].getNPC()
@@ -46,41 +50,60 @@ if __name__ == "__main__":
             print()
             print()
             for x in range(len(candyBucket)):
-                print(x ,candyBucket[x].getName(),candyBucket[x].wear(), "uses left")
-            wpnOption = input("which weapon?")
+                print(x ,candyBucket[x].getName())
+                print()
+            wpnOption = input("Which weapon would you like to use?")
             int(wpnOption)
             wpnNum = candyBucket[int(wpnOption)].getWeapon()
             for n in range(len(npc)):
-                npc[n].hit(randint(10,20)* candyBucket[int(wpnOption)].getUses(), wpnNum)
+                npc[n].hit(randint(10,20)* candyBucket[int(wpnOption)].wear(), wpnNum)
             HPlost = 0
             for n in range(len(npc)):
                 hero.set_health(-npc[n].attack())
                 HPlost += npc[n].attack()
-            print("you lost ",HPlost, "health. \n remaining health: ", hero.get_health())
-           
-            
+            print()
+            print()
+            print("You lost ",HPlost, "health. \n remaining health: ", hero.get_health())
+
+
 
             if hero.get_health() < 1:
-                print("dude you died")
+                print("dude you died :( ")
+                sys.exit()
+                print()
                 break
-          
-            
+
+
 #How to end the game
         if option == "sugar coma":
             sys.exit()
-#Leave the house      
+#Leave the house
         if option == "leave":
-            print("there are", len(hood), "houses that need to be cleared")
+            print()
+            print()
+            print("There are", len(hood), "houses that need to be cleared.")
             houseNum = int(input("Which house would you like to hit? "))
             int(houseNum)
             houseNum += -1
             hood[houseNum].get_NPC()
-        
+
 
 #Check your inventory
         if option == "inventory":
             for x in range(len(candyBucket)):
+                print()
+                print()
                 print(x ,candyBucket[x].getName(),candyBucket[x].getUses(), "uses left")
 
+        if hood[houseNum].isEmpty() == 0:
+            hood.remove(hood[houseNum])
+
         else:
+            print()
+            print()
             print("Please pick a command from the list.")
+print()
+print()
+print()
+print("Congrats you little turd, you actually did it")
+print("You saved the neighborhood from the monsters!")
