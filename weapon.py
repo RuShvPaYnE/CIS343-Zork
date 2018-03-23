6560bfc0c28af8c14b0306bf868657c6315130d3
AustinVkemp <Austinvkemp@gmail.com>
2018-03-23 11:23:09 -0400

    Update weapon.py

--- weapon.py
+++ weapon.py
@@ -1,46 +1,50 @@
 from random import randint
 from observable import Observable
 
-#Austin Van Kempen and my boy Lanndon Rose
+#### Austin Van Kempen and Lanndon Rose ####
 
 
 
-
+#### Weapon Class ####
 class Weapon(Observable):
   def __init__(self, weapon):
       super().__init__()
 
-
+#### HersheyKiss weapon and properties ####
       if weapon == 0:
         self.name = "HersheyKiss"
         self.weapon = weapon
         self.uses = 1000
-
+        
+#### SourStraws weapon and properties ####
       if weapon == 1:
         self.name = "SourStraws"
         self.weapon = weapon
         self.uses = 2
-
+        
+#### ChocolateBars weapon and properties ####
       if weapon == 2:
           self.name = "ChocolateBars"
           self.weapon = weapon
           self.uses = 4
-
+          
+#### NerdBOMB weapon and properties ####
       if weapon == 3:
           self.name = "NerdBOMB"
           self.weapon = weapon
           self.uses = 1
 
+#### Icecream weapon and properties ####          
       if weapon == 4:
           self.name = "IceCream"
           self.weapon = weapon
           self.uses = 1
 
 
-#https://codereview.stackexchange.com/questions/138085/text-based-rpg-using-oop
+#### https://codereview.stackexchange.com/questions/138085/text-based-rpg-using-oop ####
+#### Went on the website above and it gave a lot of useful info ####
 
-#Went on the website above and it gave a lot of useful info
-
+#### Damage for each weapon ####
   def wear(self):
       if self.weapon == 0:
         self.uses = self.uses - 1
